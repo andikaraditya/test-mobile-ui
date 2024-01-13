c4ba86d44e4b34a1d662bd84bb69449e4808e516
Andika <andikaraditya1@gmail.com>
2024-01-13 17:06:43 +0700

    add drag scroll on product section

--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -1,3 +1,6 @@
+import { useRef } from "react"
+import { useDraggable } from "react-use-draggable-scroll";
+
 const products = [
     {
         img: "https://nilaigizi.com/assets/images/produk/produk_1578041377.jpg",
@@ -42,6 +45,9 @@ const products = [
 ]
 
 function ProductSection() {
+    const ref = useRef()
+    const { events } = useDraggable(ref)
+
     return (
         <>
             <div className="flex justify-between px-5 mt-16 items-center">
@@ -51,7 +57,11 @@ function ProductSection() {
                 </div>
                 <p className="text-orange-600 font-bold text-xl">SEE ALL</p>
             </div>
-            <div className="overflow-x-scroll">
+            <div 
+            id="product-container" 
+            ref={ref} 
+            {...events}
+            className="overflow-x-scroll">
                 <div 
                 className="flex mt-5 gap-[20px] px-[20px] py-5"
                 style={{width: `calc(${products.length} * 220px + 20px) `}}
@@ -67,14 +77,14 @@ function ProductSection() {
 
 function ProductCard({ item }) {
     return (
-        <div className="w-[200px]">
+        <div className="w-[200px] prevent-select">
             <img
                 src={item.img}
-                className="border-2 rounded-xl w-[200px] h-[200px] object-fill"
+                className="border-2 rounded-xl w-[200px] h-[200px] object-fill prevent-select"
                 alt=""
             />
-            <p className="font-bold mt-3">{item.name}</p>
-            <p className="text-orange-600 font-bold">{item.point} Pts</p>
+            <p className="font-bold mt-3 prevent-select">{item.name}</p>
+            <p className="text-orange-600 font-bold prevent-select">{item.point} Pts</p>
         </div>
     )
 }
